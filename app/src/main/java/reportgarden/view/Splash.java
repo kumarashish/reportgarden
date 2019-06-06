b64680cc5cf65ec37032c0c7a5eb00a49ea4e9ba
AshishDmss <ashishk@dmss.co.in>
2019-06-06 14:05:43 +0530

    first commit

--- app/src/main/java/reportgarden/view/Splash.java
+++ app/src/main/java/reportgarden/view/Splash.java
@@ -32,7 +32,7 @@ public class Splash extends AwesomeSplash {
         //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
 
         //Customize Logo
-        configSplash.setLogoSplash(R.drawable.logo); //or any other drawable
+        configSplash.setLogoSplash(R.drawable.logo_new); //or any other drawable
         configSplash.setAnimLogoSplashDuration(1000); //int ms
         configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
 
